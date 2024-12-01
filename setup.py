784fc9adbd9eb72e0202ac22222654e35797935c
laynia <vc_mikuu@hotmail.com>
2024-12-01 18:09:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="beatpy",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python wrapper for the BeatSaver API",
     author="laynia",
     author_email="vc.mikuu@outlook.jp",
