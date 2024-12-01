0444cd67808abb2e4a794caade285303566f3608
laynia <vc_mikuu@hotmail.com>
2024-12-01 18:13:00 +0100

    Update __init__.py

--- beatpy/__init__.py
+++ beatpy/__init__.py
@@ -1,2 +1,19 @@
+
 from .api import BeatSaverAPI
-from .exceptions import BeatSaverError, BeatSaverNotFoundError, BeatSaverAuthError
+from .exceptions import (
+    BeatSaverError,
+    BeatSaverNotFoundError,
+    BeatSaverAuthError,
+    BeatSaverRateLimitError,
+    BeatSaverServerError,
+)
+
+
+__all__ = [
+    "BeatSaverAPI",
+    "BeatSaverError",
+    "BeatSaverNotFoundError",
+    "BeatSaverAuthError",
+    "BeatSaverRateLimitError",
+    "BeatSaverServerError",
+]
